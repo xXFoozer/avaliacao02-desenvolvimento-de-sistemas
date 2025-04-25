49cc20a9b2e0783e09236a5b32cb15f758c1dd88
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2025-04-24 21:52:23 -0300

    Prova : (

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
-import { useEffect } from 'react';
+'use client'
+import { FormEvent, useEffect, useState } from 'react';
+import axios, { Axios } from 'axios'
+import { v4 as uuid } from 'uuid'
 
 export default function MarketList() {
   useEffect(() => {
     loadItens();
   }, [])
 
+  
 
-  function loadItens() {
-    // BUSCA AS INFORMAÇÕES NA API FAKE
-    // SALVA O VALOR NO ESTADO
+  async function loadItens() {
+    const response = await axios.get("http://localhost:3001/itens")
+    
   }
 
-  function handleAddItem() {
-    // CRIAR O OBJETO DO ITEM
-    // CHAMA A API PARA ADICIONAR O ITEM
-    // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
+  async function handleAddItem(event: FormEvent) {
+    event.preventDefault()
+
+    const itens = {
+      id: uuid(),
+      text: String,
+      checked: false
+    }
+
+    await axios.post("http://localhost:3001/itens", itens)
+
+    await loadItens();
   }
 
   function handleRemoveItem(id: string) {
@@ -23,13 +35,35 @@ export default function MarketList() {
     // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
   }
 
-  function handleUpdateItem(id: string) {
+  async function handleUpdateItem(id: string) {
     // CRIA O OBJETO DO ITEM
+    const itens = {
+      id: id,
+      text: String,
+      checked: true
+    }
     // CHAMA A API PARA ATUALIZAR O ITEM
-    // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
+    await axios.patch('http://localhost:3001/itens', itens.id)
+    // CARREGA OS PRODUTOS NOVAMENTE // 
+    loadItens();
   }
 
   return (
-    <h1>MarketList</h1>
+    <div>
+
+      <h1>Lista de compra</h1>
+      <div className="grupo-input">
+        <input type="text" id="tarefa-text" placeholder="Adicionar uma nova tarefa"/>
+          <button id="add-tarefa" onClick={handleAddItem}>
+            <span className="material-symbols-outlined">
+              add
+            </span>
+          </button>
+      </div>
+
+      <ul id="lista-tarefa">
+        {/* {itens.map(itens)} */}
+      </ul>
+    </div>
   );
 }
